b7fa6e0f5fff5f0a91543a8ba4cae7fdb7a4454b
Burak Kaygusuz <burakkaygusuz@outlook.com>
2025-07-10 22:31:06 +0300

    feat(config): add @Component annotation to ScannerConfig

--- src/main/java/io/github/burakkaygusuz/config/ScannerConfig.java
+++ src/main/java/io/github/burakkaygusuz/config/ScannerConfig.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.validation.annotation.Validated;
  */
 @ConfigurationProperties(prefix = "scanner")
 @Validated
+@Component
 public class ScannerConfig {
 
   /** SQL injection payloads for testing */
