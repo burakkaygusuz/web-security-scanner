cb7a446ebd5a8be3db428a16a689a5599df50b7b
Burak Kaygusuz <burakkaygusuz@outlook.com>
2025-07-10 22:05:31 +0300

    refactor(tests): update ConfigLoaderTest to use external properties file and add application-test.yml for configuration

--- src/test/java/io/github/burakkaygusuz/config/ConfigLoaderTest.java
+++ src/test/java/io/github/burakkaygusuz/config/ConfigLoaderTest.java
@@ -9,27 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(
-    properties = {
-      "scanner.sqlPayloads[0]='",
-      "scanner.sqlPayloads[1]=1' OR '1'='1",
-      "scanner.sqlPayloads[2]=' OR 1=1--",
-      "scanner.sqlPayloads[3]=' UNION SELECT NULL--",
-      "scanner.xssPayloads[0]=<script>alert('XSS')</script>",
-      "scanner.xssPayloads[1]=<img src=x onerror=alert('XSS')>",
-      "scanner.xssPayloads[2]=javascript:alert('XSS')",
-      "scanner.xssPayloads[3]=<svg onload=alert('XSS')>",
-      "scanner.sensitivePatterns.email=[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}",
-      "scanner.sensitivePatterns.phone=\\b\\d{3}[-.]?\\d{3}[-.]?\\d{4}\\b",
-      "scanner.sensitivePatterns.ssn=\\b\\d{3}-\\d{2}-\\d{4}\\b",
-      "scanner.sensitivePatterns.api_key=api[_-]?key[\\s]*[:=][\\s]*['\"]?[a-zA-Z0-9]+['\"]?",
-      "scanner.sensitivePatterns.credit_card=\\b\\d{4}[\\s-]?\\d{4}[\\s-]?\\d{4}[\\s-]?\\d{4}\\b",
-      "scanner.scanSettings.maxDepth=3",
-      "scanner.scanSettings.timeoutSeconds=30",
-      "scanner.csrfSettings.testForms=true",
-      "scanner.csrfSettings.checkSameSiteCookies=true",
-      "scanner.csrfSettings.minimumTokenLength=16"
-    })
+@TestPropertySource(locations = "classpath:test-application.properties")
 class ConfigLoaderTest {
 
   @Autowired private ScannerConfig scannerConfig;
